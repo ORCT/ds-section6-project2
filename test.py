8e1c2e2c94de686b6eaa524a2ad8ca02a3337c77
ORCT <sonic407@naver.com>
2023-02-15 10:58:45 +0900

    new

---
+++ test.py
@@ -0,0 +1,6 @@
+import torch
+import test_setting
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+feat = test_setting.Feature
---
+++ test_setting.py
@@ -0,0 +1,5 @@
+class Feature:
+    model_path = './reid_onnx_deploy/models/vehicleid.onnx'
+    size = (256, 256)
+    img_path = './reid_onnx_deploy/sample'
+    
